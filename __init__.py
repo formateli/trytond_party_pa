5e8c255cc3130ca2c7ffa0034e89c78dae504cc3
formateli <framirez@formateli.com>
2026-05-19 16:44:14 +0000

    Tryton 7.8

--- __init__.py
+++ __init__.py
@@ -1,11 +1,3 @@
 # This file is part of party_pa module.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import Pool
-from . import configuration
-
-
-def register():
-    Pool.register(
-        configuration.Configuration,
-        module='party_pa', type_='model')
